292ac4b40566c4b4995d61fc8a45b0fe80a8863d
stutisk <48529714+stutisk@users.noreply.github.com>
2023-03-10 14:53:18 +0530

    feat:added marquee effect

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,19 +1,22 @@
-import { Cards2Container, Cards2Image, Cards2text } from "../Styles/Card.style";
+import {
+  Cards2Container,
+  Cards2Image,
+  Cards2text,
+  Cardmarqueetext,
+} from "../Styles/Card.style";
 import ReactPlayer from "react-player";
 
 const cards = [
   {
     image:
       "https://res.cloudinary.com/dvbw8xujd/image/upload/v1678435181/aflyuyrfatyfzemnhxut.jpg",
-      title:"ESFI Announces National Esports"
+    title: "ESFI Announces National Esports",
   },
   {
     image:
       "https://res.cloudinary.com/dvbw8xujd/image/upload/v1678435319/mkesrc9xylnbnyvplj4r.jpg",
-      title:"Indian LAN Gaming Announced Season 4 of ILG Cup"
+    title: "Indian LAN Gaming Announced Season 4 of ILG Cup",
   },
-
- 
 ];
 
 export const Cards2 = () => {
@@ -29,6 +32,11 @@ export const Cards2 = () => {
               className="border-radius"
             />
           </Cards2Image>
+          <Cardmarqueetext >
+            <div className="marqueeText">
+            {card.title}
+            </div>
+          </Cardmarqueetext>
           <Cards2text>{card.title}</Cards2text>
         </Cards2Container>
       ))}
--- src/Styles/Card.style.js
+++ src/Styles/Card.style.js
@@ -1,44 +1,45 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CardContainer = styled.div`
-height: 200px;
-
+  height: 200px;
 `;
 export const CardsContainer = styled.div`
-height: 150px;
-display: flex;
-gap:6px;
-width:154px;
-flex-wrap: wrap;
-
+  height: 150px;
+  display: flex;
+  gap: 6px;
+  width: 154px;
+  flex-wrap: wrap;
 `;
 
 export const Cards2Container = styled.div`
-height: 150px;
-display: flex;
+  height: 150px;
+  display: flex;
 
-width: 100%;
-padding:7px;
-background-color: #ff8c00;
-border-radius: 10px;
-flex-direction: column;
-
-
+  width: 100%;
+  padding: 7px;
+  background-color: #ff8c00;
+  border-radius: 10px;
+  flex-direction: column;
 `;
 
 export const Cards2Image = styled.div`
-    height: 85px;
-    width: 100%;
-  
-
+  height: 85px;
+  width: 100%;
 `;
 export const Cards2text = styled.div`
-   width: 170px;
-   -webkit-line-clamp: 2;
-   overflow: hidden;
-   display: -webkit-box;
-   -webkit-box-orient: vertical;
-   font-size: 1rem;
+  width: 170px;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  font-size: 1rem;
   font-weight: 300;
+`;
+export const Cardmarqueetext = styled.div`
+  font-size: 1rem;
+  font-weight: 700;
+  height: 25px;
 
+  overflow: hidden;
+  position: relative;
 `;
